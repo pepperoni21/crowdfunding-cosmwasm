d0f58415939fb351ce658470a0512ce7a2f84473
pepperoni21 <pariselias00@gmail.com>
2023-12-18 17:56:40 +0100

    Fixed formatting

--- src/contract/query/list_projects.rs
+++ src/contract/query/list_projects.rs
@@ -4,8 +4,7 @@ use crate::{error::CrowdfundingError, project::Project, state::PROJECTS};
 
 pub fn list_projects(deps: Deps) -> StdResult<Binary> {
     let projects = PROJECTS.load(deps.storage)?;
-    let projects = projects.into_values()
-        .collect::<Vec<Project>>();
+    let projects = projects.into_values().collect::<Vec<Project>>();
     Ok(Binary::from(
         serde_json::to_string(&projects)
             .map_err(CrowdfundingError::SerializationError)?
